3b7d630cb023eb9f96ab3ec57dc15a79c5653f50
ViniciusMendes <viniciusmferreira70@gmail.com>
2021-02-26 13:56:09 +0000

    (10/02) Início da revisão de JS

--- revisao-js/vetores.js
+++ revisao-js/vetores.js
@@ -25,3 +25,38 @@ console.log(primeiraFruta)
 let terceiraFruta = frutas.splice(2, 1) //Fruta na posição 2
 console.log(frutas)
 console.log(terceiraFruta) // splice() sempre retorna um vetor
+
+
+// Inserindo um elemento no fim do vetor
+frutas.push('jabuticaba')
+
+
+console.log(frutas)
+
+//Inserir elementos no início do vetor
+frutas.unshift('amora')
+
+console.log(frutas)
+
+
+// Inserindo em posição intermediaria
+//splice() para inserção
+// 1° Parâmetro: a posição onde ocorrerá a inserção
+// 2° Parâmetro: quantos elementos serão excluídos (0)
+// Parâmetros seguintes: elementos a serem inseridos
+
+// Inserindo na 4° posição
+frutas.splice(3, 0, 'pêssego')
+console.log(frutas)
+
+
+// Inserindo duas frutas na terceira posição
+frutas.splice(2, 0, 'caquí', 'jaca')
+console.log(frutas)
+
+
+// Substituindo a 6° fruta
+frutas.splice(5, 1, 'abacate')
+console.log(frutas)
+
+
