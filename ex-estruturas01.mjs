8b2dc93b3459881a4bbeefc9e1e7efa530b895a6
ViniciusMendes <viniciusmferreira70@gmail.com>
2021-05-24 12:30:46 +0000

    (24/05/2021) Tabelas de espelhamento

--- ex-estruturas01.mjs
+++ ex-estruturas01.mjs
@@ -3,3 +3,10 @@
     um número decimal em um número binário.
     Data de entrega: 26/05, até 11h20, valendo nota de participação
 */
+
+import { Stack } from "./lib/resolucao-exercicio1.mjs";
+
+let pilha = new Stack()
+
+let num = '3'
+
